f69be4589cc9084f87593ee26c74e7bdf96169c6
ganirockz <ganeshrapeti8@gmail.com>
2020-09-24 17:04:03 +0530

    Added usecase2

--- src/com/capgemini/SnakeAndLadder.java
+++ src/com/capgemini/SnakeAndLadder.java
@@ -5,5 +5,7 @@ public class SnakeAndLadder {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to snake and Ladder");
 		int position = 0;
+		int number_on_dice = (int) Math.floor(Math.random()*10)%6 +1;
+		System.out.println("The number on the dice is: "+number_on_dice);
 	}
 }
