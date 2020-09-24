2f7e0bf16e172f584ec11f841247bced59937b4c
ganirockz <ganeshrapeti8@gmail.com>
2020-09-24 17:13:13 +0530

    Added usecase6

--- src/com/capgemini/SnakeAndLadder.java
+++ src/com/capgemini/SnakeAndLadder.java
@@ -4,9 +4,10 @@ public class SnakeAndLadder {
 	public static int noPlay =0,ladder = 1,snake =2;
 	public static void main(String[] args) {
 		System.out.println("Welcome to snake and Ladder");
-		int position = 0;
+		int position = 0, number_of_turns =0;
 		while(position < 100) {
 		int number_on_dice = (int) Math.floor(Math.random()*10)%6 +1;
+		number_of_turns ++;
 		System.out.println("The number on the dice is: "+number_on_dice);
 		int option = (int) Math.floor(Math.random()*10)%3;
 		if(option == noPlay) {
@@ -22,7 +23,9 @@ public class SnakeAndLadder {
 			else
 				position = 0;
 		}
+		System.out.println("The position of player is: "+position);
 		}
 		System.out.println("The final postion reached by the player is: "+position);
-	}
+		System.out.println("The number of times the dice rolled is: "+number_of_turns);
+	}	
 }
